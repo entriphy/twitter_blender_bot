f41ca4fe1698c2a74cdfe57f2e68d5cf92a451ed
entriphy <1747088+entriphy@users.noreply.github.com>
2023-11-10 16:18:15 -0800

    Add support for Klonoa Beach Volleyball

--- blender.py
+++ blender.py
@@ -19,16 +19,31 @@ bpy.context.scene.frame_end = int(action.curve_frame_range[1]) # Set length of r
 bpy.context.scene.camera = camera # Set active camera to randomly selected camera
 
 # Klonoa-specific stuff. Comment or delete this code if you do not need it.
-split = action.name.split("_")
-idx = split[0]
-action_name = "_".join(split[1:-1])
-mesh_actions: list[bpy.types.Action] = [a for a in bpy.data.actions if "Armature" not in a.name]
-for mesh_action in mesh_actions:
-    # Apply blend shape animations (if any)
-    if mesh_action.name.startswith(f"{idx}_"):
-        part = mesh_action.name.split("_")[-1]
-        mesh: bpy.types.Mesh = bpy.data.objects[part].data
-        mesh.shape_keys.animation_data.action = mesh_action
+if len(bpy.path.basename(bpy.context.blend_data.filepath).split(".")[0]) == 2: # Klonoa Beach Volleyball
+    # Get index of UV map to use
+    split = action_name.split(" ") # Format: "anim_name (index)"
+    action_name = split[0]
+    channel = int(split[1][1]) 
+
+    # Set UV map for each mesh
+    meshes: list[bpy.types.Object] = [m for m in bpy.data.objects if m.type == "MESH"]
+    for mesh in meshes:
+        layers = list(mesh.data.uv_layers)
+        if channel < len(layers):
+            layers[channel].active_render = True
+        else:
+            layers[0].active_render = True
+else: # Klonoa 2
+    split = action.name.split("_")
+    idx = split[0]
+    action_name = "_".join(split[1:-1])
+    mesh_actions: list[bpy.types.Action] = [a for a in bpy.data.actions if "Armature" not in a.name]
+    for mesh_action in mesh_actions:
+        # Apply blend shape animations (if any)
+        if mesh_action.name.startswith(f"{idx}_"):
+            part = mesh_action.name.split("_")[-1]
+            mesh: bpy.types.Mesh = bpy.data.objects[part].data
+            mesh.shape_keys.animation_data.action = mesh_action
 
 # Render to out directory
 bpy.context.scene.render.filepath = os.path.join(os.getcwd(), "out", f"{action_name}.mp4")
